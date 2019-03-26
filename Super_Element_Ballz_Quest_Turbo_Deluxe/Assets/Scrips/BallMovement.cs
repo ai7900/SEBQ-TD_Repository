35acc92ccbd34b3edc26c8e471a22aa00851a1c6
Mattias Ståhlberg <46970089+ai7900@users.noreply.github.com>
2019-03-26 02:31:30 +0100

    Added CameraFollow, BallMovement, ect.
    The camera now follows the Player in an orthographic mode.
    The player can move. But it's shite. The scrip needs to be impoved.

---
+++ Super_Element_Ballz_Quest_Turbo_Deluxe/Assets/Scrips/BallMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    [SerializeField]
+    public float moveSpeed = 5000f;
+
+    Vector3 forward, right;
+
+    private void Start()
+    {
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+
+        right = Quaternion.Euler(new Vector3(0,90, 0)) * forward;
+
+        
+    }
+
+    private void Update()
+    {
+
+        //if (Input.anyKey)
+        //    Move();
+
+        Vector3 xSpeed = Input.GetAxis("Horizontal") * right * moveSpeed * Time.deltaTime;
+        Vector3 ySpeed = Input.GetAxis("Vertical") * forward * moveSpeed * Time.deltaTime;
+
+        transform.position += xSpeed;
+        transform.position += ySpeed;
+
+        //Rigidbody body = GetComponent<Rigidbody>();
+        //body.AddTorque(new Vector3(xSpeed, 0, ySpeed) * moveSpeed * Time.deltaTime);
+    }
+
+    //void Move()
+    //{
+    //    Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    //    Vector3 upMovement = forward * moveSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+    //    Vector3 rightMovement = right * moveSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+
+    //    //Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+    //    //transform.forward = heading;    //rotation
+
+    //    Rigidbody body = GetComponent<Rigidbody>();
+    //    body.AddTorque(rightMovement + upMovement);
+
+    //    //transform.position += rightMovement;
+    //    //transform.position += upMovement;
+    //}
+
+}
