16d047c46f8b64c4296d2b0cba5c72549ba75e06
Mattias Ståhlberg <46970089+ai7900@users.noreply.github.com>
2019-04-10 18:02:34 +0200

    FireBall added

--- Super_Element_Ballz_Quest_Turbo_Deluxe/Assets/Scripts/Player/BallModeController.cs
+++ Super_Element_Ballz_Quest_Turbo_Deluxe/Assets/Scripts/Player/BallModeController.cs
@@ -10,6 +10,7 @@ public class BallModeController : MonoBehaviour
     public GameObject normalBallPrefab;
     public GameObject heavyBallPrefab;
     public GameObject lightBallPrefab;
+    public GameObject FireBallPrefab;
 
     private GameObject newObject;
  
@@ -17,7 +18,7 @@ public class BallModeController : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetKeyDown(KeyCode.Keypad1))
         {
             
             newObject=Instantiate(normalBallPrefab, gameObject.transform.position, gameObject.transform.rotation);
@@ -25,16 +26,23 @@ public class BallModeController : MonoBehaviour
             Destroy(gameObject);
         }
 
-        else if (Input.GetButtonDown("Fire2"))
+        else if (Input.GetKeyDown(KeyCode.Keypad2))
         {
             newObject = Instantiate(heavyBallPrefab, gameObject.transform.position, gameObject.transform.rotation);
             newObject.GetComponent<Rigidbody>().velocity = gameObject.GetComponent<Rigidbody>().velocity;
             Destroy(gameObject);
         }
 
-        else if (Input.GetButtonDown("Fire3"))
+        else if (Input.GetKeyDown(KeyCode.Keypad3))
         {
             newObject = Instantiate(lightBallPrefab, gameObject.transform.position, gameObject.transform.rotation);
+            newObject.GetComponent<Rigidbody>().velocity = gameObject.GetComponent<Rigidbody>().velocity;
+            Destroy(gameObject);
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            newObject = Instantiate(FireBallPrefab, gameObject.transform.position, gameObject.transform.rotation);
             newObject.GetComponent<Rigidbody>().velocity = gameObject.GetComponent<Rigidbody>().velocity;
             Destroy(gameObject);
         }
