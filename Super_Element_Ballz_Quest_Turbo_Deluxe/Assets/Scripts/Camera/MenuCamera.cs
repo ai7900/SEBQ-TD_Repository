6c785d1484b5ab543f55b98c42ccdf9af2db6c33
Reaper93 <48824425+Reaper93Z@users.noreply.github.com>
2019-04-12 12:02:42 +0200

    Snyggade till menyn och lade till scripts

--- Super_Element_Ballz_Quest_Turbo_Deluxe/Assets/Scripts/Camera/MenuCamera.cs
+++ Super_Element_Ballz_Quest_Turbo_Deluxe/Assets/Scripts/Camera/MenuCamera.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MenuCamera : MonoBehaviour
 {
+    public enum MenuState {Play = 0, Continue, Level_Select, Options, Quit};
+
     [SerializeField]
     [Range(0.01f, 10f)]
     private float transitionSpeed;
@@ -13,38 +15,61 @@ public class MenuCamera : MonoBehaviour
 
     private Transform currentViewTransform;
     private Vector3 currentAngle;
-    private int activeView;
+
+    public MenuState CurrentMenuState { get; private set; }
 
     // Start is called before the first frame update
     private void Start()
     {
-        currentViewTransform = views[0];
+        currentViewTransform = views[(int)MenuState.Play];
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.DownArrow))
+        if(Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            activeView--;
+            CurrentMenuState--;
+            if(CurrentMenuState < 0)
+            {
+                CurrentMenuState = MenuState.Quit;
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            activeView++;
+            CurrentMenuState++;
+            if((int)CurrentMenuState == views.Length)
+            {
+                CurrentMenuState = MenuState.Play;
+            }
         }
     }
 
     // Update is called once per frame
     private void LateUpdate()
     {
-        currentViewTransform = views[activeView];
-        transform.position = Vector3.Lerp(transform.position, currentViewTransform.position, transitionSpeed * Time.deltaTime);
+        currentViewTransform = views[(int)CurrentMenuState];
+        SmoothMovementToNewPos();
+        SmoothRotateToNewPos();
+    }
 
+    private void SmoothRotateToNewPos()
+    {
         currentAngle = new Vector3(
-                    Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentViewTransform.eulerAngles.x, Time.deltaTime * transitionSpeed),
-                    Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentViewTransform.eulerAngles.y, Time.deltaTime * transitionSpeed),
-                    Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentViewTransform.eulerAngles.z, Time.deltaTime * transitionSpeed));
+                   Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentViewTransform.eulerAngles.x, Time.deltaTime * transitionSpeed),
+                   Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentViewTransform.eulerAngles.y, Time.deltaTime * transitionSpeed),
+                   Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentViewTransform.eulerAngles.z, Time.deltaTime * transitionSpeed));
 
         transform.eulerAngles = currentAngle;
+    }
+
+    private void SmoothMovementToNewPos()
+    {
+        transform.position = Vector3.Lerp(transform.position, currentViewTransform.position, transitionSpeed * Time.deltaTime);
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(20, Screen.height - 30, 200, 200), "Current state = " + CurrentMenuState.ToString());
     }
 }
---
+++ Super_Element_Ballz_Quest_Turbo_Deluxe/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuHandler : MonoBehaviour
+{
+    public MenuCamera menuCamera;
+
+    [SerializeField]
+    private string firstLevel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Lägg till funktioner för de olika menyvalen här
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            switch (menuCamera.CurrentMenuState)
+            {
+                case MenuCamera.MenuState.Play:
+                    {
+                        //Vi använder sceneFader här sen!
+                        SceneManager.LoadScene(firstLevel);
+                    }
+                    
+                    break;
+
+                case MenuCamera.MenuState.Continue:
+                    {
+
+                    }
+                    break;
+
+                case MenuCamera.MenuState.Level_Select:
+                    {
+
+                    }
+                    break;
+
+                case MenuCamera.MenuState.Options:
+                    {
+
+                    }
+                    break;
+
+                case MenuCamera.MenuState.Quit:
+                    {
+                        Debug.Log("Game Quit");
+                        Application.Quit();
+                    }
+                    break;
+            }
+        }
+            
+
+        //if(Input.GetKeyDown(KeyCode.Return))
+        //{
+        //    if(menuCamera.CurrentMenuState == MenuCamera.MenuState.Play)
+        //    {
+                
+        //    }
+
+        //    else if (menuCamera.CurrentMenuState == MenuCamera.MenuState.Continue)
+        //    {
+
+        //    }
+
+        //    else if (menuCamera.CurrentMenuState == MenuCamera.MenuState.Level_Select)
+        //    {
+
+        //    }
+
+        //    else if (menuCamera.CurrentMenuState == MenuCamera.MenuState.Options)
+        //    {
+
+        //    }
+
+        //    else if (menuCamera.CurrentMenuState == MenuCamera.MenuState.Quit)
+        //    {
+
+        //    }
+        //}
+    }
+}
