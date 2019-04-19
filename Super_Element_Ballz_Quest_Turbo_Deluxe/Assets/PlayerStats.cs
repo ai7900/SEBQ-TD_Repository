14dbfb0bd8e9585c51612d6dd68caf3e2a2f5a60
Reaper93 <48824425+Reaper93Z@users.noreply.github.com>
2019-04-19 16:41:32 +0200

    Lade till collectible, funktion, modell och PlayerStats script
    PlayerStats ska kopplas till en riktig UI sen

---
+++ Super_Element_Ballz_Quest_Turbo_Deluxe/Assets/PlayerStats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [HideInInspector]
+    public static int CollectiblesPickedUp;
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(20, 100, 100, 100), "Collectibles picked up " + CollectiblesPickedUp);
+    }
+}
