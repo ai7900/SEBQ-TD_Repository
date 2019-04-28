2d661e54820db8a8cafb037ddc55489dca3d8f48
DanielFornell <danielfornell@hotmail.com>
2019-04-28 17:38:33 +0200

    Ny third person Kamera att Testa

---
+++ Super_Element_Ballz_Quest_Turbo_Deluxe/Assets/Scripts/Camera/CameraFollow2.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2 : MonoBehaviour
+{
+
+    public float cameraMoveSpeed = 120.0f;
+    public GameObject cameraFollowObject;
+    Vector3 followPos;
+    public float clampAngle = 80.0f;
+    public float inputSensitivity = 150.0f;
+    public GameObject cameraObj;
+    public GameObject playerObj;
+    public float camDistanceXToPlayer;
+    public float camDistanceYToPlayer;
+    public float camDistanceZToPlayer;
+    public float mouseX;
+    public float mouseY;
+    public float finalInputX;
+    public float finalInputZ;
+    public float smoothX;
+    public float smoothY;
+    private float rotY = 0.0f;
+    private float rotX = 0.0f;
+
+    void Start()
+    {
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("RightStickHorizontal");
+        float inputZ = Input.GetAxis("RightStickVertical");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+        finalInputX = (inputX + mouseX) * inputSensitivity;
+        finalInputZ = (inputZ + mouseY) * inputSensitivity;
+        rotY += finalInputX * Time.deltaTime;
+        rotX += finalInputZ * Time.deltaTime;
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
+    }
+
+    void LateUpdate()
+    {
+        CameraUpdater();
+    }
+    void CameraUpdater()
+    {
+        Transform target = cameraFollowObject.transform;
+        float step = cameraMoveSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
