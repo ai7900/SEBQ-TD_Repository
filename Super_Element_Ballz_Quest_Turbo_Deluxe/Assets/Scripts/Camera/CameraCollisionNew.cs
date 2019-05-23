0c76805d9bc6f18b3b1f7f271a9dcca0b6149a18
Reaper93 <48824425+Reaper93Z@users.noreply.github.com>
2019-05-23 17:01:44 +0200

    Fixade kamera kollision

--- Super_Element_Ballz_Quest_Turbo_Deluxe/Assets/Scripts/Camera/CameraCollisionNew.cs
+++ Super_Element_Ballz_Quest_Turbo_Deluxe/Assets/Scripts/Camera/CameraCollisionNew.cs
@@ -23,16 +23,22 @@ public class CameraCollisionNew : MonoBehaviour
     void Update()
     {
         Vector3 desiredCameraPos = transform.parent.TransformPoint(dollyDir * maxDistance);
-        //RaycastHit hit;
+        RaycastHit hit;
 
-        //if(Physics.Linecast (transform.parent.position, desiredCameraPos, out hit))
-        //{
-        //    distance = Mathf.Clamp((hit.distance * 0.9f), minDistance,maxDistance);
-        //}
-        //else
-        //{
-        //    distance = maxDistance;
-        //}
+        if (Physics.Linecast(transform.parent.position, desiredCameraPos, out hit))
+        {
+
+            if (hit.collider.CompareTag("Environment"))
+            {
+                distance = Mathf.Clamp(hit.distance * 0.9f, minDistance, maxDistance);
+            }
+            else return;
+
+        }
+        else
+        {
+            distance = maxDistance;
+        }
         transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDir * distance, Time.deltaTime * smooth);
         
     }
