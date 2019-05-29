7a2093141ece8c4157182ea2088d7433e2c642a3
Mattias Ståhlberg <46970089+ai7900@users.noreply.github.com>
2019-05-29 20:42:29 +0200

    Added a whole new level with scrips 'n' all

---
+++ Super_Element_Ballz_Quest_Turbo_Deluxe/Assets/Scripts/Traps/HalfRotatingPlatform.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfRotatingPlatform : MonoBehaviour
+{
+
+    private float rotationSpeed;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rotationSpeed = 0.4f;
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, 0, 0);
+    }
+
+    private IEnumerator PlatformMovement()
+    {
+
+        transform.Rotate(0, rotationSpeed, 0, Space.World);
+
+        if (transform.rotation.eulerAngles.y > 180)
+        {
+            rotationSpeed *= -1;
+            yield return new WaitForSeconds(2);
+        }
+
+        if (transform.rotation.eulerAngles.y < 0)
+        {
+            rotationSpeed *= -1;
+            yield return new WaitForSeconds(2);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(PlatformMovement());
+    }
+}
