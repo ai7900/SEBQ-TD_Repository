06ff58cb1f80413f1d38e109d0d4adadac34d92a
Reaper93 <48824425+Reaper93Z@users.noreply.github.com>
2019-05-30 16:53:01 +0200

    iskuben gör nu is när den är i vatten..
    det är inte snyggt, men det funkar. Gjorde så att man kan vara is längre och kan röra sig lite bättre i vatten

---
+++ Super_Element_Ballz_Quest_Turbo_Deluxe/Assets/Scripts/UI/PausMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausMenu : MonoBehaviour
+{
+    public static bool pausedGame;
+    [SerializeField]
+    private GameObject pauseMenuUi;
+    
+    
+    void Start()
+    {
+        pausedGame = false;
+        pauseMenuUi.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pausedGame)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1f;
+        pausedGame = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenuUi.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0f;
+        pausedGame = true;
+    }
+}
